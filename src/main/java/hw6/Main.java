b942919bb8f153ac71004ff8fcaa1f75ba3a98af
JoeSemper <joe.semper@gmail.com>
2020-08-03 10:53:50 +0300

    Commit

---
+++ src/main/java/hw6/Main.java
@@ -0,0 +1,14 @@
+package hw6;
+
+import com.sun.xml.internal.ws.message.ByteArrayAttachment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+    }
+}
